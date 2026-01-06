b56b50cabf76a52a95b69c9b16b5355ff588468a
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2026-01-06 16:15:47 -0600

    ArrayElementSearch and neetcodeblinds merged and presented as new package

---
+++ SolveSomething/src/main/java/me.solvesomething/ArrayElementSearch.java
@@ -0,0 +1,30 @@
+package me.solvesomething;
+
+import java.util.*;
+
+public class ArrayElementSearch {
+
+    int[] elements = {5, 4, 3, 2, 1};
+
+
+    public String searchElement(int findElement) {
+
+        for (int x : elements) {
+            if (findElement == x) {
+                return "Search successful - element found";
+            }
+        }
+        return "Search failed - element not found";
+
+    }
+
+    public static void main(String[] args) {
+
+        ArrayElementSearch object = new ArrayElementSearch();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an element to find...");
+        System.out.println(object.searchElement(input.nextInt()));
+
+    }
+
+}
