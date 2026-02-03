ce77b6abe79b5e834a1c60171bde0177366173fc
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2026-02-03 10:10:23 -0600

    removed unnecessary things and simplified.

--- SolveSomething/src/main/java/i/solvesomething/ArrayMaxMinDiff.java
+++ SolveSomething/src/main/java/i/solvesomething/ArrayMaxMinDiff.java
@@ -2,35 +2,23 @@ package i.solvesomething;
 
 public class ArrayMaxMinDiff {
 
-    public int findMaxMinDiff(int arr[]) {
-        int max=arr[0];
-        int min=arr[0];
-        int diff;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] < arr[j]) {
-                    max = arr[arr.length - 1];
-                    min = arr[0];
-                    diff = min-max;
-                    return diff;
-                } else {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    max = arr[arr.length - 1];
-                    min = arr[0];
-                    diff = min-max;
-                    return diff;
-                }
+    public int findMaxMinDiff(int[] arr) {
+        int max = arr[0];
+        int min = arr[0];
 
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
-        return 0;
+        return max - min;
     }
 
     public static void main(String[] args) {
-
         ArrayMaxMinDiff object = new ArrayMaxMinDiff();
-        System.out.println(object.findMaxMinDiff(new int[]{6,7,3,2}));
+        System.out.println(object.findMaxMinDiff(new int[]{6, 7, 3, 2}));
     }
 }
