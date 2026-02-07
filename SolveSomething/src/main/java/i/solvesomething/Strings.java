e54728c23e59d1ea7c67d0c48416d3a1c430c212
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2026-02-07 16:11:58 -0600

    Printing a simple static string.

---
+++ SolveSomething/src/main/java/i/solvesomething/Strings.java
@@ -0,0 +1,13 @@
+package i.solvesomething;
+
+
+public class Strings {
+
+    static String name ="ISHU";
+
+    public static void main(String[] args) {
+
+        System.out.println("How is life going "+name+ "?");
+        System.out.println("My life here is so bad!");
+    }
+}
